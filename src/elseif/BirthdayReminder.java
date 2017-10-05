d0abafcf7b2ad802094576a0fa80bc4f9fc3af35
JTablas <Jason.Tablas@gompersstudent.org>
2017-10-05 14:25:12 -0700

    Day 1052017

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,21 +11,31 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "April 26th";
+		String dadsBirthday = "June 1st";
+		
+		String myBirthday = "July 13th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String birth= JOptionPane.showInputDialog("what birthday do you want to know");
 		// 3. Print out what the user typed
-		
+		System.out.println(birth);
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if(birth.equals("mom"));{
+			System.out.println(momsBirthday);
+		}
 		// 5. if user asked for "dad"
+		if(birth.equals("dad"));{
+			System.out.println(dadsBirthday);
+		}
 			// print dad's birthday
+		if(birth.equals("Chris"));{
+			System.out.println(myBirthday);
+		}
 		// 6. if user asked for your name
 			// print myBirthday
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		
 	} 
 }
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,23 +5,57 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
+		Robot timmy = new Robot();
+		for(int a = 0; a < 100; a++){
 		//3. ask the user what color they would like the robot to draw
-		
+		String colors = JOptionPane.showInputDialog("what color do you want?");
 		//4. use an if/else statement to set the pen color that the user requested
+		timmy.setSpeed(10);
+		timmy.setPenWidth(10);
 
-        //5. if the user doesn’t enter anything, choose a random color
-
+		if(colors.equals("blue")){
+			timmy.setPenColor(30, 144, 225);
+		}
+		if(colors.equals("red")){
+			timmy.setPenColor(Color.RED);
+		}
+		if(colors.equals("green")){
+			timmy.setPenColor(Color.GREEN);
+		}
+		if(colors.equals("orange")){
+			timmy.setPenColor(Color.ORANGE);
+		}
+		if(colors.equals("purple")){
+			timmy.setPenColor(Color.MAGENTA);
+		}
+		if(colors.equals("pink")){
+			timmy.setPenColor(Color.PINK);
+		}
+		if(colors.equals("yellow")){
+			timmy.setPenColor(Color.YELLOW);
+		}
+		
+		//5. if the user doesn’t enter anything, choose a random color
+		if(colors.equals(""))
+		
         //6. put a loop around your code so that you keep asking the user for more colors & drawing them
 		
 		//2. set the pen width to 10
-		
+		timmy.setPenWidth(10);
 	    //1. make the robot draw a shape (this will take more than one line of code)
-
-
+		
+		timmy.penDown();
+		for(int i = 0; i < 4; i++){
+		timmy.move(100);
+		timmy.turn(90);
+		}
 	}
+}
 }
